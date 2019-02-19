7f1d7a2d4a46320c76f8e9be11ed269db16293bf
Kristian O'Brien <kristianobrien95@gmail.com>
2019-02-19 06:02:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var recipes = new Object ({})
+var recipes = new Object({});
 
- var obj = { prop: 1 }
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},obj,{[key]:value})
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+return object
 }
-function deleteFromObjectByKey(object) {
-  delete object.key;
-  return obj
-}
-function destructivelyDeleteFromObjectByKey(object, key) {
+
+ function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
+  return newObj
+
+ }
+
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
   return object
 }
